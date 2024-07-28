83f090966c796b9bb7586f25771dcaf48036f54c
Osher Rachamim <62746945+osherachamim@users.noreply.github.com>
2024-07-28 15:14:21 +0300

    Create guess_the_secret_number.py

---
+++ guess_the_secret_number.py
@@ -0,0 +1,23 @@
+secret_number = 10
+
+print(
+"""
++================================+
+| Welcome to my game, muggle!    |
+| Enter an integer number        |
+| and guess what number I've     |
+| picked for you.                |
+| So, what is the secret number? |
++================================+
+""")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    
+    if guess == secret_number:
+        print("Congratulations! You've guessed the secret number!")
+        break
+    elif guess < secret_number:
+        print("Too low! Try again.")
+    else:
+        print("Too high! Try again.")
